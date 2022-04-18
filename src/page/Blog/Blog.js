8149d9c2852a9d1c05b35e307956fe119e025df6
jasimahmedq2 <jasimahmed90852417>
2022-04-18 23:12:35 +0600

    about-added

--- src/page/Blog/Blog.js
+++ src/page/Blog/Blog.js
@@ -2,8 +2,35 @@ import React from 'react';
 
 const Blog = () => {
   return (
-    <div>
-      blog
+    <div className='container'>
+      <div>
+        <h2>authorizetion vs authentication</h2>
+        <h5>
+          Authentication is the act of validating that users are whom they claim to be. This is the first step in any security process. Authorization
+          system security is the process of giving the user permission to access a specific resource or function. This term is often used interchangeably with access control or client privilege.
+          Despite the similar-sounding terms, authentication and authorization are separate steps in the login process. Understanding the difference between the two is key to successfully implementing an IAM solution.
+        </h5>
+      </div>
+      <br />
+      <div>
+        <h2>why I am use firebase</h2>
+         <h5>
+        Firebase Authentication  to make building secure authentication systems.  while improving the sign-in and onboarding experience for end users. It provides  identity solution, supporting email and password accounts, phone auth, and Google, Twitter, Facebook, and GitHub login, etc.
+        authentication withuot farebase 
+        </h5>
+      </div>
+      <br />
+
+      <div>
+        <h2>firebase service list withuot authentication</h2>
+        <h5>
+          firebase many service provided withuot authentication
+          Cloud Firestore,Hosting, Cloud Storage, Google Analytics , Predictions, Cloud ,Dynamic Links
+          Messaging, Remote Config, Cloud Function
+
+
+        </h5>
+      </div>
     </div>
   );
 };
--- src/page/Home/About/About.js
+++ src/page/Home/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import jasim from '../../../page/image/jasim.png'
+import './About.css'
 
 const About = () => {
   return (
     <div>
-      about
+      <div className='w-50 mx-auto mb-5 img-container'><img  src={jasim} alt="" /></div>
+      <div className='container'>
+        <h3>
+        Iam jasim
+        Now Iam learn Front-end web development. My goal after 5-6 month become
+        A full stack  web developer. Then I will join entern or job.I will Try hard work and my best giving this course.
+        </h3>
+        </div>
     </div>
   );
 };
